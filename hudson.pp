983b5df2efc4e464d7c2bd6ec3c705e9de29b8da
Chris May <chris.may@warwick.ac.uk>
2011-07-20 14:01:14 +0100

    hudson group

--- hudson.pp
+++ hudson.pp
@@ -4,9 +4,13 @@ user { 'hudson':
     home => '/var/hudson',
     uid => '1001',
     ensure => 'present',
-    gid => '1001',
+    gid => '1002',
     groups => ['adm','admin',],
     comment => 'Hudson user,,,'
+}
+
+group {"hudson":
+    gid=>1002
 }
 
 file{["/var/hudson","/var/hudson/.ssh"]: ensure=>directory, owner=>hudson}
