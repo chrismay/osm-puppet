60dda89cafc0317723be3ad19b9deeae108d422f
Chris May <chris.may@warwick.ac.uk>
2011-07-20 13:55:44 +0100

    add hudson stuff

--- init.pp
+++ init.pp
@@ -1,11 +1,13 @@
-$mapnik_user="vagrant"
-$mapnik_home_dir="/home/vagrant"
+$mapnik_user="hudson"
+$mapnik_home_dir="/var/${mapnik_user}"
 $osm2pgsql_install_dir="${mapnik_home_dir}/bin/osm2pgsql"
+$shared_data_dir="/data"
+$config_dir="/data"
 
 class packages{
 package{[subversion,autoconf,screen,htop]: ensure=>present}
 
-file{["/home/vagrant/src","/home/vagrant/bin","/home/vagrant/planet"]: ensure=>directory}
+file{["${mapnik_home_dir}/src","${mapnik_home_dir}/bin","${mapnik_home_dir}/planet"]: ensure=>directory}
 
 
 package{["postgresql-8.4-postgis", "postgresql-contrib-8.4","postgresql-server-dev-8.4",
@@ -17,33 +19,34 @@ package{["mapnik-utils","python-mapnik"]:ensure=>present}
 
 class osm-build{
 exec{"/usr/bin/svn co http://svn.openstreetmap.org/applications/utils/export/osm2pgsql/":
-    cwd=>"/home/vagrant/bin",
-    creates=>"/home/vagrant/bin/osm2pgsql",
+    cwd=>"${mapnik_home_dir}/bin",
+    creates=>"${mapnik_home_dir}/bin/osm2pgsql",
     alias=>"svn-get-osm2pgsql"
 }
 
-exec{"/home/vagrant/bin/osm2pgsql/autogen.sh && /home/vagrant/bin/osm2pgsql/configure && /usr/bin/make":
-    cwd=>"/home/vagrant/bin/osm2pgsql",
+exec{"${mapnik_home_dir}/bin/osm2pgsql/autogen.sh && ${mapnik_home_dir}/bin/osm2pgsql/configure && /usr/bin/make":
+    cwd=>"${mapnik_home_dir}/bin/osm2pgsql",
     logoutput=>true,
     require=>Exec["svn-get-osm2pgsql"],
-    creates=>"/home/vagrant/bin/osm2pgsql/osm2pgsql",
+    creates=>"${mapnik_home_dir}/bin/osm2pgsql/osm2pgsql",
     alias=>"build-osm2pgsql"
 }
 }
 class mapnik-build{
     exec{"/usr/bin/svn co  http://svn.openstreetmap.org/applications/rendering/mapnik":
-        cwd=>"/home/vagrant/bin",
-        creates=>"/home/vagrant/bin/mapnik",
+        cwd=>"${mapnik_home_dir}/bin",
+        creates=>"${mapnik_home_dir}/bin/mapnik",
+        user=>"${mapnik_user}",
         alias=>"svn-get-mapnik-tools"
     }
-    file{"/home/vagrant/bin/mapnik/world_boundaries": ensure=>"/vagrant/osm_data/world_boundaries"}
+    file{"${mapnik_home_dir}/bin/mapnik/world_boundaries": ensure=>"${shared_data_dir}/osm_data/world_boundaries"}
 }
 class postgres{
     service{"postgresql-8.4": ensure=>running}
-    file{"/etc/postgresql/8.4/main/postgresql.conf": source=>"/vagrant/postgresql.conf",
+    file{"/etc/postgresql/8.4/main/postgresql.conf": source=>"${shared_data_dir}/postgresql.conf",
        notify=>Service["postgresql-8.4"]
     }
-    exec{"/usr/bin/createuser -s ${mapnik_user} && /usr/bin/createdb -E UTF8 -O ${mapnik_user} --template template0 gis && /usr/bin/createlang plpgsql gis && touch /var/lib/postgresql/puppet_made_users":
+    exec{"/usr/bin/createuser -s ${mapnik_home_dir} && /usr/bin/createdb -E UTF8 -O ${mapnik_home_dir} --template template0 gis && /usr/bin/createlang plpgsql gis && touch /var/lib/postgresql/puppet_made_users":
         creates=>"/var/lib/postgresql/puppet_made_users",
         user=>postgres,
         logoutput=>true,
@@ -56,7 +59,7 @@ class postgres{
         alias=>"enable-postgis",
         require=>Exec["create-postgis-users"]
     }
-    exec{"/bin/echo \"ALTER TABLE geometry_columns OWNER TO ${mapnik_user}; ALTER TABLE spatial_ref_sys OWNER TO ${mapnik_user};\" | /usr/bin/psql -d gis && touch /var/lib/postgresql/puppet_fixed_owners":
+    exec{"/bin/echo \"ALTER TABLE geometry_columns OWNER TO ${mapnik_home_dir}; ALTER TABLE spatial_ref_sys OWNER TO ${mapnik_home_dir};\" | /usr/bin/psql -d gis && touch /var/lib/postgresql/puppet_fixed_owners":
         creates=>"/var/lib/postgresql/puppet_fixed_owners",
         user=>postgres,
         logoutput=>true,
@@ -70,7 +73,7 @@ class postgres{
         require=>Exec["enable-postgis"]
     }
 
-    exec{"/usr/bin/psql -f /home/vagrant/bin/osm2pgsql/900913.sql -d gis && touch /var/lib/postgresql/puppet_enabled_srid":
+    exec{"/usr/bin/psql -f ${mapnik_home_dir}/bin/osm2pgsql/900913.sql -d gis && touch /var/lib/postgresql/puppet_enabled_srid":
         creates=>"/var/lib/postgresql/puppet_enabled_srid",
         user=>postgres,
         logoutput=>true,
@@ -80,7 +83,7 @@ class postgres{
 }
 
 class import-osm{
-exec{"/home/vagrant/bin/osm2pgsql/osm2pgsql -S default.style --slim -d gis -C 2048 /vagrant/nec-gw-gf-2011.osm.bz2 && touch /var/lib/postgresql/puppet_imported_nec":
+exec{"${mapnik_home_dir}/bin/osm2pgsql/osm2pgsql -S default.style --slim -d gis -C 2048 ${shared_data_dir}/nec-gw-gf-2011.osm.bz2 && touch /var/lib/postgresql/puppet_imported_nec":
     user=>postgres,
     logoutput=>true,
     creates=>"/var/lib/postgresql/puppet_imported_nec",
