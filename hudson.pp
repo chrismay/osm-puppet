2af34b32b66669863ee85b2dd517e0ff056c3f21
Chris May <chris.may@warwick.ac.uk>
2011-07-20 14:46:04 +0100

    add packages

--- hudson.pp
+++ hudson.pp
@@ -17,7 +17,7 @@ file{["/var/hudson","/var/hudson/.ssh"]: ensure=>directory, owner=>hudson}
 
 }
  ssh_authorized_key{"hudson@bob":
-    key=>"AAAAB3NzaC1yc2EAAAABIwAAAQEAxXoZHNd6A/CmfR2uPM9YvgE9P4dEfkL+hMNXbPcGp8B82eJ/LtPgDh3M1BIGoXjZAgMUwafdGcCcF4rCVxjAluc7v9wzwIgALR9ILL+JJWxqUdVzZtES0nuk3NI55cO6Z/YZIYXpxdYxWY7nUmKSHuf3iePpspFdL7K4houQIqnkfalUybhwZ7sjaQRbBMsIQlgLwKBWnbp1eGKq1VRRs6q+FwNcqhTsLhfQBAU8pKTwt2qCWh430kZTUv7x8cZcbyiur09SWotjmhON742ke+LEhUxVvfZykO2LU8Wm5OZL2UYKFe1KRJ86ePXiPZkkTYjZUWldAsaXk9ihowE2Lw"
+    key=>"AAAAB3NzaC1yc2EAAAABIwAAAQEAxXoZHNd6A/CmfR2uPM9YvgE9P4dEfkL+hMNXbPcGp8B82eJ/LtPgDh3M1BIGoXjZAgMUwafdGcCcF4rCVxjAluc7v9wzwIgALR9ILL+JJWxqUdVzZtES0nuk3NI55cO6Z/YZIYXpxdYxWY7nUmKSHuf3iePpspFdL7K4houQIqnkfalUybhwZ7sjaQRbBMsIQlgLwKBWnbp1eGKq1VRRs6q+FwNcqhTsLhfQBAU8pKTwt2qCWh430kZTUv7x8cZcbyiur09SWotjmhON742ke+LEhUxVvfZykO2LU8Wm5OZL2UYKFe1KRJ86ePXiPZkkTYjZUWldAsaXk9ihowE2Lw",
     type=>"ssh-rsa",
     user=>"hudson",
     target=>"/var/hudson/.ssh/authorized_keys",
